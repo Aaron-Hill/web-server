24b43ed337e3e59b4d350c20414886fd566c437e
Aaron Hill <Finch659@gmail.com>
2016-05-08 20:37:09 -0400

    Initialize repo

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+
+var PORT = 3000;
+
+var middleware = {
+	requireAuthentication: function(req, res, next) {
+		console.log('Private route hit');
+		next();
+	},
+	logger: function(req, res, next) {
+		console.log('Request: ' + req.method + ' ' + req.originalUrl + ' at ' + new Date().toString());
+		next();
+	}
+}
+
+// app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+	res.send("About us!");
+});
+
+app.use(express.static(__dirname + '/public'));
+
+//TO PRINT THE FILE NAME 
+// console.log(__dirname);
+
+app.listen(PORT, function() {
+	console.log("Server is listening at port " + PORT);
+});
+
+
+
